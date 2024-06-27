e382a914e4239c3f871b5f502e33f0ca077fe6c5
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2024-06-27 09:57:17 -0300

    Init

---
+++ bsn-api/src/test/java/com/correa/bsn/BsnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.correa.bsn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsnApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
